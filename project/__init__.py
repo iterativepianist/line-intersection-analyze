eb12c930c1bae00a05cf2ca49305d3dd7ef0b6fd
Klimowicz <marcin.klimowicz@iterative.pl>
2022-06-23 21:32:14 +0200

    Clear class

--- project/__init__.py
+++ project/__init__.py
@@ -25,9 +25,6 @@ def create_app():
         # since the user_id is just the primary key of our user table, use it in the query for the user
         return User.query.get(int(user_id))
 
-    # blueprint for auth routes in our app
-    from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint)
 
     # blueprint for non-auth parts of app
     from .main import main as main_blueprint
