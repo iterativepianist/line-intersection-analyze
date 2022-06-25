c1a82ca2fa224377069a502e9cd7ed2905b31829
lhyla <lhylacontact@gmail.com>
2022-06-25 13:01:48 +0200

    More or less acceptable solution
    Not all edge cases yet covered.

--- project/main.py
+++ project/main.py
@@ -1,6 +1,8 @@
+import os
+
 from flask import Blueprint, render_template, request
 from flask_login import login_required, current_user
-
+from project import lineintersection
 
 main = Blueprint('main', __name__)
 
@@ -12,27 +14,52 @@ def index():
 
 @main.route('/data')
 def data():
-    headings = ["Średnia", "Mediana"]
-    data = [
-        ["150", "250"],
-        ["350", "350"]
-    ]
-
-    return render_template('data.html', headings=headings, data=data)
+    return render_template('data.html')
 
 
 @main.route('/calculate', methods=['POST'])
 def calculate():
     # First line
-    p1_x_1 = request.form.get('1-P1-X')
-    p1_y_1 = request.form.get('1-P1-Y')
-    p2_x_1 = request.form.get('1-P2-X')
-    p2_y_1 = request.form.get('1-P2-Y')
+    p1_x_1 = int(request.form.get('P1-X'))
+    p1_y_1 = int(request.form.get('P1-Y'))
+    p2_x_1 = int(request.form.get('P2-X'))
+    p2_y_1 = int(request.form.get('P2-Y'))
 
     # Second line
-    p1_x_2 = request.form.get('2-P1-X')
-    p1_y_2 = request.form.get('2-P1-Y')
-    p2_x_2 = request.form.get('2-P2-X')
-    p2_y_2 = request.form.get('2-P2-Y')
+    p1_x_2 = int(request.form.get('P3-X'))
+    p1_y_2 = int(request.form.get('P3-Y'))
+    p2_x_2 = int(request.form.get('P4-X'))
+    p2_y_2 = int(request.form.get('P4-Y'))
 
-    return render_template('data.html')
+    start1 = [p1_x_1, p1_y_1]
+    end1 = [p2_x_1, p2_y_1]
+    start2 = [p1_x_2, p1_y_2]
+    end2 = [p2_x_2, p2_y_2]
+
+    result = lineintersection.lineIntersec(
+        start1,
+        end1,
+
+        start2,
+        end2
+    )
+
+    print("result", str(result))
+
+    if start1 == end1 and end1 == start2 and start2 == end2:
+        data = "Odcinki leżą w tym samym punkcie"
+    elif start1 == start2 and end1 == end2:
+        data = "Odcinki są takie same"
+    elif isinstance(result, tuple):
+        data = "Odcinki przecinają się w punkcie x:" + str(result[0]) + " y:" + str(result[1])
+    elif isinstance(result, list):
+        data = "Odcinki mają wspólny odcinek P5:" + str(result[0]) + " P6:" + str(result[1])
+    elif start1 == start2 or start1 == end2:
+        data = "Odcinki mają wspólny punkt x:" + str(start1[0]) + " y:" + str(start1[1])
+    elif end1 == start2 or end1 == end2:
+        data = "Odcinki mają wspólny punkt x:" + str(start1[0]) + " y:" + str(start1[1])
+    else:
+        data = "Odcinki nie przecinają się"
+
+
+    return render_template('rendered_data.html', data=data)
